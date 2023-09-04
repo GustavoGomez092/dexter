9596148c609d83098ddcfae5441b68ab5208317e
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-03 19:00:34 -0600

    ✨ created new headers and footer components for the challenges & main routes

---
+++ components/ChallengeFooter/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChallengeFooter() {
+  return (
+    <div className='text-center text-gray-600 py-5'>
+      <p>Dexter - Code Challenges for Job applicants ⓒ Copyright 2023</p>
+    </div>
+  )
+}
