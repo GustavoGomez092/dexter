38b6d7a17ab061328371cf3fc24181d91ce09b30
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-03 18:57:47 -0600

    💄 Cleaned up main entry css

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-h-[100vh] flex-col items-center justify-between p-24">
-
+    <main className="flex flex-col items-center">
+      <Header />
+      <div className="container flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold text-center">Welcome to Dexter</h1>
+        <p className="text-xl text-center">A code challenge platform for job applicants</p>
+      </div>
+      <Footer />
     </main>
   )
 }
