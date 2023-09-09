776d1c9490367b1bea44c47ce0f3dacc5f6943cb
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-09 10:36:58 -0600

    💄 Updated countdown styles

--- components/CountDown/index.tsx
+++ components/CountDown/index.tsx
@@ -1,24 +1,24 @@
 
 export default function index() {
   return (
-    <div className="flex flex-col">
-      <p className="text-gray-500 mb-2">Time left:</p>
-      <div className="timer flex gap-2 text-[12px] text-center">
-      <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+    <div className="flex flex-col dark">
+      <p className="text-gray-500 mb-1">Time left:</p>
+      <div className="timer flex gap-4 text-[12px] text-center">
+      <div className="flex flex-col rounded-box text-text">
     <span className="countdown font-mono text-xl">
-      <span style={{"--value":1}}></span>
+      <span>00</span>
     </span>
     hours
   </div> 
-  <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+  <div className="flex flex-col rounded-box text-text">
     <span className="countdown font-mono text-xl">
-      <span style={{"--value":2}}></span>
+      <span>12</span>
     </span>
     min
   </div> 
-  <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+  <div className="flex flex-col rounded-box text-text">
     <span className="countdown font-mono text-xl">
-      <span style={{"--value":42}}></span>
+      <span>55</span>
     </span>
     sec
   </div>
