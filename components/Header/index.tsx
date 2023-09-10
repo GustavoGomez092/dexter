28aa7e0f8f1df1f86e6aaf2f2d1b26fa7abcfd87
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-10 16:10:30 -0600

    💄 Removed grid from the Main site header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import CountDown from '@/components/CountDown'
 import './style.css'
 export default function Header() {
   return (
-    <div className='header h-24 container mx-auto relative flex justify-between items-center'>
+    <div className='h-24 container mx-auto relative flex justify-between items-center'>
       <div className="absolute left-0 w-full h-full" />
       <div className="logo-area h-full z-20 relative w-fit">
         <img src="/assets/img/logo.svg" className="h-full py-6" alt="logo" />
