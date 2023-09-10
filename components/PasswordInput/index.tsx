31f2d4463fc80f506d99d9129634ed3eb6fe629b
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-10 00:11:03 -0600

    ➕ Created Password input component

---
+++ components/PasswordInput/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Input} from "@nextui-org/react";
+import {EyeFilledIcon} from "./EyeFilledIcon";
+import {EyeSlashFilledIcon} from "./EyeSlashFilledIcon";
+
+export default function PasswordInput(props:any) {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <Input
+      {...props}
+      endContent={
+        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+          {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )}
+        </button>
+      }
+      type={isVisible ? "text" : "password"}
+    />
+  );
+}
