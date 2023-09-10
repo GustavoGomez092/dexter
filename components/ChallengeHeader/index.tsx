2d0d3ce3f999bde9806c56223031b82e44c63da7
Gustavo Gomez <gustavogomez092@gmail.com>
2023-09-10 00:10:13 -0600

    ♻️ Removed whitespace

--- components/ChallengeHeader/index.tsx
+++ components/ChallengeHeader/index.tsx
@@ -3,7 +3,7 @@ import './style.css'
 export default function ChallengeHeader() {
   return (
     <div className='header h-24 container mx-auto relative flex justify-between items-center'>
-      <div className="absolute left-0 w-full h-full bg-gradient-to-b from-gray-900/0 to-gray-900/100 " />
+      <div className="absolute left-0 w-full h-full bg-gradient-to-b from-gray-900/0 to-gray-900/100" />
       <div className="logo-area h-full z-20 relative w-fit">
         <div className="logo-blur rounded-full w-36 h-6 absolute bg-teal-500 blur-xl left-12 top-10 opacity-40" />
         <img src="/assets/img/logo.svg" className="h-full py-6" alt="logo" />
