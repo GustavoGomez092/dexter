56b781700390b47bfe8a593ca90b4ae05580a682
Gustavo Gomez <gustavogomez092@gmail.com>
2024-04-02 00:14:32 -0600

    :zap: persisting applicant store data

--- store/applicantStore.ts
+++ store/applicantStore.ts
@@ -2,17 +2,27 @@ import { create } from 'zustand'
 import { Applicant, ApplicantStoreType } from './applicantStore.type'
 import { auth } from '@/providers/firebase'
 import { signOut } from 'firebase/auth'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
-export const applicantStore = create<ApplicantStoreType>((set, get) => ({
-  name: '',
-  email: '',
-  inviteId: '',
-  loading: true,
-  logIn: (applicantData:Applicant) => set(() => ({ name: applicantData.name, email: applicantData.email, inviteId: applicantData.inviteId, loading: false})),
-  isLoggedIn: () => get().inviteId !== '' ? true : false,
-  logOut: async () => {
-    await signOut(auth)
-    set(() => ({ applicant: { name: '', email: ''}, inviteId: ''}))
-  },
-  setLoading: (loading: boolean) => set(() => ({ loading: loading })),
-}))
+
+export const applicantStore = create(
+  persist(
+    (set, get):ApplicantStoreType => ({
+      name: '',
+      email: '',
+      inviteId: '',
+      loading: true,
+      logIn: (applicantData:Applicant) => set(() => ({ name: applicantData.name, email: applicantData.email, inviteId: applicantData.inviteId, loading: false})),
+      isLoggedIn: () => (get() as ApplicantStoreType).inviteId !== '' ? true : false,
+      logOut: async () => {
+        await signOut(auth)
+        set(() => ({ applicant: { name: '', email: ''}, inviteId: ''}))
+      },
+      setLoading: (loading: boolean) => set(() => ({ loading: loading })),
+    }),
+    {
+      name: 'dexter-challenge-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+)
