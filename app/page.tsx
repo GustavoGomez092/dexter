f87e189f2fcae74174d3d3f73a6e8781c4e66113
Gustavo Gomez <gustavogomez092@gmail.com>
2024-05-07 22:00:20 -0600

    :zap: redirecting user if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Spinner } from '@nextui-org/react';
 import { useAuthVerifier } from '@/hooks/useAuthVerifier';
+import { redirect } from 'next/navigation';
+import { authStore } from '@/store/authStore';
 
 export default function Home() {
   useAuthVerifier();
+  const userData = authStore((state) => state.user);
+  if (userData) {
+    redirect('/admin/challenges');
+  }
 
   return (
     <main className='flex h-screen w-screen flex-col items-center justify-between bg-gray-900 dark'>
